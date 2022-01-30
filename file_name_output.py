81045f5313b9390115886d90c6b13679eb12fa6b
napa <shino.satoru@gmail.com>
2022-01-30 22:54:50 +0900

    楽曲名を一括取得するファイルです

---
+++ file_name_output.py
@@ -0,0 +1,27 @@
+import os
+
+path = "./DP14"
+files = os.listdir(path)
+
+
+new_files = []
+for title in files:
+    new_files.append('"' + (title) + '",')
+
+for new_title in new_files:
+    print(new_title)
+    
+
+with open("music_name_for_list.txt","w",encoding="utf-8") as f:
+    f.writelines(new_files)
+
+# f = open("music_name_for_list.csv","w",encoding="utf-8")
+
+# f.write(str(files))
+
+# f.close()
+
+
+
+
+# print(type(files))
