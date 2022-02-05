8cc73d2e150ed0e324f5cb092930d9fa173a95a1
napa <shino.satoru@gmail.com>
2022-02-05 15:44:16 +0900

    曲の名前を改行して取得するように変更

--- file_name_output.py
+++ file_name_output.py
@@ -6,14 +6,16 @@ files = os.listdir(path)
 
 new_files = []
 for title in files:
-    new_files.append('"' + (title) + '",')
+    new_files.append('data.add("' + (title) + '");')
 
 for new_title in new_files:
     print(new_title)
-    
 
-with open("music_name_for_list.txt","w",encoding="utf-8") as f:
-    f.writelines(new_files)
+
+
+with open("music_name_for_list.txt","w",encoding="utf-8") as f:   
+    for d in new_files:
+        f.write("%s\n" % d)
 
 # f = open("music_name_for_list.csv","w",encoding="utf-8")
 
