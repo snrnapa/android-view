d6f7238efe0b2ba601ee3fde19d504d63c1b4c5b
napa <shino.satoru@gmail.com>
2022-02-16 16:22:08 +0900

    pythonファイルのタイトル、ファイル名取得に分別しました

--- python_file/file_name_output.py
+++ python_file/file_name_output.py
@@ -4,6 +4,9 @@ path = "DP14"
 files = os.listdir(path)
 
 
+# これより下は譜面のタイトルを成形するコードです。
+
+
 new_files = []
 for title in files:
     new_files.append('"' + (title.replace(".html","")) + '",')
@@ -14,9 +17,37 @@ for new_title in new_files:
 
 
 
-with open("music_name_for_list.txt","w",encoding="utf-8") as f:   
+with open("MusicName.txt","w",encoding="utf-8") as f:   
     for d in new_files:
         f.write("%s\n" % d)
+
+# これより上は譜面のタイトルを成形するコードです。
+
+
+
+
+# これより下は譜面のタイトルを成形するコードです。
+
+
+new_files = []
+for title in files:
+    new_files.append('R.drawable.' + (title) + ',')
+
+for new_title in new_files:
+    print(new_title)
+
+
+
+
+with open("FileName.txt","w",encoding="utf-8") as f:   
+    for d in new_files:
+        f.write("%s\n" % d)
+
+# これより上は譜面のタイトルを成形するコードです。
+
+
+
+
 
 # f = open("music_name_for_list.csv","w",encoding="utf-8")
 
